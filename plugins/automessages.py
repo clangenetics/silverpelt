a079982296da262d64a216a7a244e101e38eb9a1
Luna R <lunarnightskies@gmail.com>
2023-05-10 16:55:46 +0000

    Add auto messages, like carl

---
+++ plugins/automessages.py
@@ -0,0 +1,21 @@
+import hikari
+import lightbulb
+import ujson
+
+messages = ujson.load(open("automessages.json", "r"))
+
+plugin = lightbulb.Plugin("automsgs")
+
+@plugin.listener(hikari.GuildMessageCreateEvent)
+async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
+    if event.message.author.is_bot:
+        return
+    if event.message.content in messages.keys():
+        await event.message.respond(messages[event.message.content])
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+
+def unload(bot):
+    bot.remove_plugin(plugin)
