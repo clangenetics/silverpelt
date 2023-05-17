706cbeef776ff167b79799f09d76a790fda36772
Luna R <lunarnightskies@gmail.com>
2023-05-17 15:50:20 +0000

    add basic save correction

--- plugins/checksave.py
+++ plugins/checksave.py
@@ -13,6 +13,7 @@ from extensions.checks import techhelp_only
 plugin = lightbulb.Plugin("checksave")
 
 nullregex = r"(?:\x00)+"
+sha1regex = r"[a-fA-F\d]{40}"
 
 
 @plugin.command
@@ -61,16 +62,26 @@ async def checksave(ctx: lightbulb.Context) -> None:
 
     if not os.path.exists(f"{filedir}/saves"):
         if not os.path.exists(f"{filedir}/currentclan.txt"):
-            await ctx.respond("No save file found")
-            return
-        with open(f"{filedir}/currentclan.txt", "r") as f:
-            clan = f.read()
-        searchdir = filedir
+            if os.listdir(filedir)[0]:
+                if os.path.isdir(f"{filedir}/{os.listdir(filedir)[0]}"):
+                    clan = os.listdir(filedir)[0]
+                    searchdir = filedir + "/" + clan
+                else:
+                    await ctx.respond("No save file found")
+                    return
+            else:
+                await ctx.respond("No save file found")
+                return
+        else:
+            with open(f"{filedir}/currentclan.txt", "r") as f:
+                clan = f.read()
+            searchdir = filedir
     else:
         with open(f"{filedir}/saves/currentclan.txt", "r") as f:
             clan = f.read()
         searchdir = f"{filedir}/saves"
 
+    attemptedToFix = False
     if re.match(nullregex, clan):
         criticals.append("`currentclan.txt` is nullified")
 
@@ -82,66 +93,95 @@ async def checksave(ctx: lightbulb.Context) -> None:
             await ctx.respond("No save file found")
             return
 
+        with open(f"{searchdir}/currentclan.txt", "w") as f:
+            f.write(clan)
+        attemptedToFix = True
+
+        
+
     info['Clan Name'] = f"{clan}clan"
 
+    clanjson = None
     if not os.path.exists(f"{searchdir}/{clan}clan.json"):
         criticals.append(f"Missing `{clan}clan.json`")
+    else:
+        try:
+            with open(f"{searchdir}/{clan}clan.json", "r") as f:
+                clandata = f.read()
+            clanjson = ujson.loads(clandata)
+            info['Clan Version'] = clanjson['version_commit']
+        except ujson.JSONDecodeError:
+            if re.match(nullregex, clandata):
+                criticals.append(f"`{clan}clan.json` is nullified")
+            else:
+                criticals.append(f"`{clan}clan.json` is not valid json")
+        searchdir = f"{searchdir}/{clan}"
+
 
     try:
-        with open(f"{searchdir}/{clan}clan.json", "r") as f:
-            clandata = f.read()
-        clanjson = ujson.loads(clandata)
-        info['Clan Version'] = clanjson['version_commit']
-    except ujson.JSONDecodeError:
-        if re.match(nullregex, clandata):
-            criticals.append(f"`{clan}clan.json` is nullified")
-        else:
-            criticals.append(f"`{clan}clan.json` is not valid json")
-
-    try:
-        with open(f"{searchdir}/{clan}/clan_cats.json", "r") as f:
+        with open(f"{searchdir}/clan_cats.json", "r") as f:
             catdata = f.read()
         catjson = ujson.loads(catdata)
 
         cats = {}
         for cat in catjson:
             cats[cat['ID']] = cat
+            cats[cat['ID']]['name'] = f"{cat['name_prefix']}{cat['name_suffix']}"
 
-        for cat in catjson:
+        for id, cat in cats.items(): # pylint: disable=redefined-builtin
 
-            if cat['ID'] not in clanjson['clan_cats']:
-                warnings.append(f"Cat `{cat['name']}` is not in clan_cats")
+
+            if clanjson and id not in clanjson['clan_cats']:
+                warnings.append(f"Cat `{cat['name']} ({id})` is not in clan_cats")
 
             # check for bad relationships
             if isinstance(cat['mate'], list):
                 for mate in cat['mate']:
                     if mate not in cats:
                         warnings.append(
-                            f"Cat `{cat['name_prefix']}{cat['name_suffix']}` has a mate that doesn't exist: `{mate}`")
+                            f"Cat `{cat['name']}({id})` has a mate that doesn't exist: `{mate}`")
 
                     if mate in [cat['parent1'], cat['parent2']]:
                         errors.append(
-                            f"Cat `{cat['name_prefix']}{cat['name_suffix']}` has a mate that is also a parent: `{mate}`")
+                            f"Cat `{cat['name']}({id})` has a mate that is also a parent: `{cats[mate]['name']} ({mate})`")
                     if mate in cat['adoptive_parents']:
                         errors.append(
-                            f"Cat `{cat['name_prefix']}{cat['name_suffix']}` has a mate that is also an adoptive parent: `{mate}`")
-            elif isinstance(cat['mate'], str):
-                if cat['mate'] not in cats:
+                            f"Cat `{cat['name']}({id})` has a mate that is also an adoptive parent: `{cats[mate]['name']} ({mate})`")
+            else:
+                if cat['mate'] and cat['mate'] not in cats:
                     warnings.append(
-                        f"Cat `{cat['name_prefix']}{cat['name_suffix']}` has a mate that doesn't exist: `{cat['mate']}`")
+                        f"Cat `{cat['name']}({id})` has a mate that doesn't exist: `{cat['mate']}`")
 
-                if cat['mate'] in [cat['parent1'], cat['parent2']]:
+                if cat['mate'] and cat['mate'] in [cat['parent1'], cat['parent2']]:
                     errors.append(
-                        f"Cat `{cat['name_prefix']}{cat['name_suffix']}` has a mate that is also a parent: `{cat['mate']}`")
-                # if cat['mate'] in cat['adoptive_parents']:
-                #     warnings.append(f"Cat `{cat['name_prefix']}{cat['name_suffix']}` has a mate that is also an adoptive parent: `{cat['mate']}`")
+                        f"Cat `{cat['name']}({id})` has a mate that is also a parent: `{cats[cat['mate']]['name']} ({cat['mate']})`")
+            
+            if cat['parent1'] and cat['parent1'] not in cats:
+                criticals.append(
+                    f"Cat `{cat['name']}({id})` has a parent1 that doesn't exist: `{cat['parent1']}`")
+                cat['parent1'] = None
+                attemptedToFix = True
+            if cat['parent2'] and cat['parent2'] not in cats:
+                criticals.append(
+                    f"Cat `{cat['name']}({id})` has a parent2 that doesn't exist: `{cat['parent2']}`")
+                cat['parent2'] = None
+                attemptedToFix = True
     except ujson.JSONDecodeError:
         if re.match(nullregex, catdata):
             criticals.append(f"`{clan}/clan_cats.json` is nullified")
         else:
             criticals.append(f"`{clan}/clan_cats.json` is not valid json")
+        
+    if attemptedToFix:
+        allcats = []
+        for cat in cats.values():
+            cat.pop('name')
+            allcats.append(cat)
+        with open(f"{searchdir}/clan_cats.json", "w") as f:
+            f.write(ujson.dumps(allcats, indent=4))
 
     def recurse(folder):
+        nonlocal attemptedToFix
         for file in os.listdir(folder):
             if os.path.isdir(f"{folder}/{file}"):
                 recurse(f"{folder}/{file}")
@@ -153,17 +193,19 @@ async def checksave(ctx: lightbulb.Context) -> None:
                 with open(f"{folder}/{file}", "r") as f:
                     data = f.read()
 
-                _folder = folder.replace(f"{searchdir}/{clan}", "")
+                _folder = folder.replace(f"{searchdir}", "")
 
                 if re.match(nullregex, data):
                     criticals.append(f"`{_folder}/{file}` is nullified")
+                    attemptedToFix = True
+                    os.remove(f"{folder}/{file}")
                 elif file.endswith('.json'):
                     try:
                         ujson.loads(data)
                     except ujson.JSONDecodeError:
                         errors.append(f"`{_folder}/{file}` is not valid json")
 
-    recurse(f"{searchdir}/{clan}")
+    recurse(f"{searchdir}")
 
     embed = hikari.Embed(
         title=f"Report for {clan}clan",
@@ -175,25 +217,59 @@ async def checksave(ctx: lightbulb.Context) -> None:
 
     if len(warnings) > 0:
         text = "\n".join(warnings)
-        if len(text) > 1024:
-            text = text[:1021] + "..."
+        while len(text) > 1024:
+            lastline = text[:1024].rfind("\n")
+            embed.add_field(name="Warnings", value=text[:lastline], inline=False)
+            text = text[lastline+1:]
         embed.add_field(name="Warnings", value=text, inline=False)
 
     if len(errors) > 0:
         embed.color = 0xFFFF00
         text = "\n".join(errors)
-        if len(text) > 1024:
-            text = text[:1021] + "..."
+        while len(text) > 1024:
+            lastline = text[:1024].rfind("\n")
+            embed.add_field(name="Errors", value=text[:lastline], inline=False)
+            text = text[lastline+1:]
         embed.add_field(name="Errors", value=text, inline=False)
 
     if len(criticals) > 0:
         embed.color = 0xFF0000
         text = "\n".join(criticals)
-        if len(text) > 1024:
-            text = text[:1021] + "..."
+        while len(text) > 1024:
+            lastline = text[:1024].rfind("\n")
+            embed.add_field(name="Criticals", value=text[:lastline], inline=False)
+            text = text[lastline+1:]
         embed.add_field(name="Criticals", value=text, inline=False)
+    
+    # Max embed length is 6000
+    # If it's over 6000, split it into multiple embeds
+    # Split so it fits as many fields as possible, but if a field is too long, put it in the next embed
 
-    await ctx.respond(embed=embed)
+    embeds = []
+    if embed.total_length() > 6000:
+        embeds.append(hikari.Embed(title=embed.title, color=embed.color))
+        for field in embed.fields:
+            if embeds[-1].total_length() + len(field.value) > 6000:
+                embeds.append(hikari.Embed(title=f"{embed.title} continued", color=embed.color))
+            embeds[-1].add_field(name=field.name, value=field.value, inline=False)
+    else:
+        embeds.append(embed)
+
+    lastmsg = None
+    for embed in embeds:
+        lastmsg = await ctx.respond(embed=embed)
+    
+
+    os.remove(f"{filedir}/{filename}")
+
+    if attemptedToFix:
+        with zipfile.ZipFile(f"{filedir}/{filename}", "w") as zip_ref:
+            for root, _, files in os.walk(filedir):
+                for file in files:
+                    if file == filename:
+                        continue
+                    zip_ref.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), filedir))
+        await ctx.respond(content="Attempted to fix saves", attachment=f"{filedir}/{filename}", reply=lastmsg)
 
     shutil.rmtree(filedir)
 
